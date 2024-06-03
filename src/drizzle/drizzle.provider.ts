41a8608eb00bb66c53ea3f4a15e7425dcf9f7fc7
aliaksandrZh <108681764+aliaksandrZh@users.noreply.github.com>
2024-06-04 00:23:34 +0300

    retry to connect to the db if failure

--- src/drizzle/drizzle.provider.ts
+++ src/drizzle/drizzle.provider.ts
@@ -1,17 +1,32 @@
 import { MySql2Database, drizzle } from 'drizzle-orm/mysql2';
 import * as mysql from 'mysql2/promise';
 import { dbCredentials } from '../../drizzle.config';
+import { catchError, firstValueFrom, from, map, retry } from 'rxjs';
 
 export type DatabaseType = MySql2Database<Record<string, never>>;
+
 const initializeDB = async () => {
-  const connection = await mysql.createConnection({
-    ...dbCredentials,
-  });
-
-  const db = drizzle(connection);
-
+  const db = await firstValueFrom(
+    from(
+      mysql.createConnection({
+        ...dbCredentials,
+      }),
+    ).pipe(
+      catchError((err) => {
+        console.log('Cannot connect to the DB.', err);
+        console.log('Next try in 5sec');
+        throw err;
+      }),
+      retry({
+        delay: 5000,
+      }),
+      map((connection) => drizzle(connection)),
+    ),
+  );
+  // const db = drizzle(connection);
   return db;
 };
+
 export type Database = MySql2Database<Record<string, never>>;
 
 export const DrizzleProvider = {
